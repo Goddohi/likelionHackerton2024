90098a6853379420c471214a6b053b5b0ce4d202
goddohi <kmam147@gmail.com>
2024-07-21 00:37:31 +0900

    [추가] BE_이지윤_기본스í최choi add springboot file

---
+++ src/main/java/com/cl/youngri/YoungriApplication.java
@@ -0,0 +1,13 @@
+package com.cl.youngri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungriApplication.class, args);
+	}
+
+}
