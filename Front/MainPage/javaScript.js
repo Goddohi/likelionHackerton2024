1567ec60dbb2909cf97bff829dba15b558da8080
jinwooYU <alal3344@naver.com>
2024-08-01 15:31:57 +0000

    메인,검색 페이지(수정)

---
+++ Front/MainPage/javaScript.js
@@ -0,0 +1,35 @@
+// 모든 overlap-2 클래스를 가진 요소들을 선택
+const overlapElements = document.querySelectorAll('.overlap-2');
+const iconElements = document.querySelectorAll('.icon');
+const imgElements = document.querySelectorAll('.img');
+const inputTextElements=document.querySelectorAll(".text-input")
+
+// 각 요소에 클릭 이벤트 리스너를 추가
+overlapElements.forEach(element => {
+    element.addEventListener('click', () => {
+        alert('클릭 이벤트가 발생했습니다!');
+        // 여기에 원하는 다른 동작을 추가할 수 있습니다.
+    });
+});
+
+iconElements.forEach(element => {
+    element.addEventListener('click', () => {
+        alert('클릭 이벤트가 발생했습니다!');
+        // 여기에 원하는 다른 동작을 추가할 수 있습니다.
+    });
+});
+
+
+imgElements.forEach(element => {
+    element.addEventListener('click', () => {
+        alert('클릭 이벤트가 발생했습니다!');
+        // 여기에 원하는 다른 동작을 추가할 수 있습니다.
+    });
+});
+
+inputTextElements.forEach(element => {
+    element.addEventListener('click', () => {
+        alert('클릭 이벤트가 발생했습니다!');
+        // 여기에 원하는 다른 동작을 추가할 수 있습니다.
+    });
+});
