37120dc2f99819139dbd6651fc53db436ae2d33d
jinwooYU <72716946+jinwiiyu@users.noreply.github.com>
2024-08-01 23:23:06 +0900

    메인,검색 페이지 약간 수정

--- SearchPage/javaScript.js
+++ SearchPage/javaScript.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const searchElements = document.querySelectorAll('.overlap');
     const deleteElements = document.querySelectorAll('.delete');
-    const backElements = document.querySelectorAll('.back'); // .back 클래스를 선택
+    const navigateElements = document.querySelectorAll('.navigate');
 
     // 검색 요소에 클릭 이벤트 리스너를 추가
     searchElements.forEach(element => {
         element.addEventListener('click', () => {
             alert('검색 이벤트가 발생했습니다!');
-            // 여기에 원하는 다른 동작을 추가할 수 있습니다.
         });
         element.addEventListener('mouseover', () => {
             const textWrapper = element.querySelector('.text-wrapper-2');
@@ -23,19 +22,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 
-    // 삭제 버튼에 클릭 이벤트 리스너를 추가
-    deleteElements.forEach(element => {
-        element.addEventListener('click', (event) => {
+    // 삭제 버튼에 클릭 및 마우스 이벤트 리스너를 추가
+    deleteElements.forEach(button => {
+        button.addEventListener('click', (event) => {
             event.stopPropagation(); // 이벤트 전파 중단
             alert('삭제 이벤트가 발생했습니다!');
-            // 여기에 원하는 다른 동작을 추가할 수 있습니다.
+        });
+
+        button.addEventListener('mouseover', () => {
+            button.style.color = 'red'; // 삭제 버튼 색상 변경
+        });
+
+        button.addEventListener('mouseout', () => {
+            button.style.color = 'white'; // 삭제 버튼 색상 변경
         });
     });
 
-    // backElements에 클릭 이벤트 리스너를 추가
-    backElements.forEach(element => {
+    // navigateElements에 클릭 이벤트 리스너를 추가
+    navigateElements.forEach(element => {
         element.addEventListener('click', () => {
-            window.location.href = 'file:///C:/Users/%EC%9C%A0%EC%A7%84%EC%9A%B0/Documents/run-react-and-next/Front_JW/index.html';
+            const targetPage = element.getAttribute('data-target');
+            window.location.href = targetPage;
         });
     });
 });
